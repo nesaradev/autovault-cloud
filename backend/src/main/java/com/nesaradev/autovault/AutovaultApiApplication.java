ba3a7a0bc48af9824331392eb5f1c7da20d6bcce
SanukaNesara <nesarasanuka4@gmail.com>
2026-08-08 16:07:03 +0530

    chore: initialize AutoVault Cloud project

---
+++ backend/src/main/java/com/nesaradev/autovault/AutovaultApiApplication.java
@@ -0,0 +1,13 @@
+package com.nesaradev.autovault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutovaultApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutovaultApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/nesaradev/autovault/AutovaultApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nesaradev.autovault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutovaultApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
